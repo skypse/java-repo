7cbe6aa7b5f34973b1f63b22b3a70a18c932146f
skypse <gabrieldoamaral.alves12@gmail.com>
2024-12-13 23:08:14 -0300

    commit primeiro projeto

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import primeiro.pacote.PrimeiraClasse;
+
+public class Main {
+    public static void main(String[] args) {
+        PrimeiraClasse primeiro = new PrimeiraClasse();
+        primeiro.mostrarMensagem();
+
+    }
+}
---
+++ src/primeiro/pacote/PrimeiraClasse.java
@@ -0,0 +1,8 @@
+package primeiro.pacote;
+
+public class PrimeiraClasse {
+
+    public void mostrarMensagem() {
+        System.out.println("Mensagem da primeira classe criada :)");
+    }
+}
